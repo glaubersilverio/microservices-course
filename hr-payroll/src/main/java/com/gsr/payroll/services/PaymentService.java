26a36082fc8d74474a84e468ec2b13b1ead65c9a
Gláuber Ribeiro <glaubersilveri@gmail.com>
2021-03-07 17:46:26 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/gsr/payroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.gsr.payroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.gsr.payroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+}
