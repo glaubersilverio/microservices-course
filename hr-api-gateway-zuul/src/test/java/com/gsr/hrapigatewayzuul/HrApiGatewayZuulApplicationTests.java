ebb13c3422d2ca9a6f3f1dac8c5d8d43bbe355ba
Gláuber Ribeiro <glaubersilveri@gmail.com>
2021-03-08 23:23:35 -0300

    Project hr-api=gateway-zuul created

---
+++ hr-api-gateway-zuul/src/test/java/com/gsr/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsr.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
