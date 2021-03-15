1ab68049a3d88a93c764c26ce41f6807fc94f4d2
Gláuber Ribeiro <glaubersilveri@gmail.com>
2021-03-14 23:52:12 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/gsr/hroauth/services/UserService.java
@@ -0,0 +1,29 @@
+package com.gsr.hroauth.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gsr.hroauth.entities.User;
+import com.gsr.hroauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		User user = this.userFeignClient.findByEmail(email).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		logger.info("Email found: " + email);
+		return user;
+	}
+	
+}
